7f3f27850554de5f929667c2b795d7bf67a30f2b
Ben Smerd <bsmerd@adam.com.au>
2022-11-21 09:41:34 +1030

    init commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// * habit tracker app with gamified aspects- similar to streaks
+
+// * use react native as want to be mobile app
+
+//* typescript and use graphql and maybe a sql database
+
+// * user creats profile and then can write out their goals they want
+
+//* they can check off a streak count for how many times they must complete this task (like streaks)
+
+// * user sets goal rewards so like after 6 months of running regularly they can buy new shoes
+
+// * create an algorithm that calculates the time frame and creates an ammount of experience earned from each completion of the task that will ultimately end up leveling them up to that timeframe level
+
+//* so if user misses 2 days of the task then it adds on 2 days until they get that reward (incentivizes not skipping or rarely skipping)
+
+// * user can have friends that also share their goals/tasks and can compare with the
+
+//* do challenges with each other based on the tasks
+
+// * share challenges with friends and do spots (cook healthy and post a photo of the meal)
+
+//* integrate strava and my fitness pal api so users can upload posts straight from there if they want
+
+// * users can comment and like posts by friends (kinda like strava)
+
+//* standard name, bio, profile pic and goals
+
+//* circle task tracker thing like streaks where easy to just save tasks
